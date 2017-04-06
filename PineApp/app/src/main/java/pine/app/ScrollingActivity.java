0cb0828f63fb6f0530f572f3a8ed7a81a14cb170
Aku <aku.jaakola@luukku.com>
2017-04-06 09:43:00 +0300

    Näyttää joko ruoat, juomat tai molemmat

--- PineApp/app/src/main/java/pine/app/ScrollingActivity.java
+++ PineApp/app/src/main/java/pine/app/ScrollingActivity.java
@@ -37,13 +37,14 @@ import java.net.URL;
 
 public class ScrollingActivity extends AppCompatActivity {
 
-    int indeksi, id, foodamount;
-    String food_name,description,imageurl;
+    int indeksi, id, foodamount, drinkamount;
+    String name,description,imageurl,tagi;
     ImageView[] imageArray = new ImageView[100];
     TextView[] textArray = new TextView[100];
     String[] descriptionArray = new String[100];
     String[] imgurlArray = new String[100];
     int[] idArray = new int[100];
+    Button buttonfood;
 
     LinearLayout.LayoutParams layoutParamsImage = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 700);
     LinearLayout.LayoutParams layoutParamsText = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 200);
@@ -53,102 +54,7 @@ public class ScrollingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
 
-        JsonTask asyncTask = (JsonTask) new JsonTask(new JsonTask.AsyncResponse() {
-            @Override
-            public void processFinish(String output) {
-
-                try {
-                    JSONArray json = new JSONArray(output);
-                    /*imageurl = json.getString("IMG_URL");
-                    description = json.getString("description");
-                    food_name = json.getString("food_name");
-                    id = json.getInt("id");*/
-
-                    LinearLayout mainLayout = (LinearLayout) findViewById(R.id.scrollingLayout);
-                    layoutParamsImage.topMargin = 100;
-
-                    foodamount = json.length();
-
-                    for(int i = 0; i < foodamount; i++) {
-                        JSONObject jsonobj = json.getJSONObject(i);
-
-                        /*System.out.println("imageurl : " + i + " = " + jsonobj.getString("IMG_URL"));
-                        System.out.println("description : " + i + " = " + jsonobj.getString("description"));
-                        System.out.println("food_name : " + i + " = " + jsonobj.getString("food_name"));
-                        System.out.println("id : " + i + " = " + jsonobj.getInt("id"));*/
-
-                        imageurl = jsonobj.getString("IMG_URL");
-                        description = jsonobj.getString("description");
-                        food_name = jsonobj.getString("food_name");
-                        id = jsonobj.getInt("id");
-
-                        ImageView foodimage = new ImageView(ScrollingActivity.this);
-                        //foodimage.setImageResource(R.drawable.maksalaatikko);
-                        imageArray[i] = foodimage;
-
-                        TextView foodtext = new TextView(ScrollingActivity.this);
-                        foodtext.setText(food_name);
-                        textArray[i] = foodtext;
-
-                        imageArray[i].setLayoutParams(layoutParamsImage);
-                        mainLayout.addView(imageArray[i]);
-
-                        textArray[i].setLayoutParams(layoutParamsText);
-                        textArray[i].setGravity(Gravity.CENTER_HORIZONTAL);
-                        mainLayout.addView(textArray[i]);
-
-                        textArray[i].setText(food_name);
-                        textArray[i].setTag(food_name);
-                        imageArray[i].setTag(food_name);
-                        textArray[i].setId(id);
-
-                        idArray[i] = id;
-                        descriptionArray[i] = description;
-                        imgurlArray[i] = (imageurl);
-
-                        Picasso.with(ScrollingActivity.this).load(imgurlArray[i]).into(imageArray[i]);
-
-                        indeksi = i;
-
-                        imageArray[i].setOnClickListener(new View.OnClickListener() {
-                            public void onClick(View v) {
-
-                                String tagi = v.getTag().toString();
-                                int etsitäänid = 0;
-
-                                while(tagi != textArray[etsitäänid].getTag())
-                                {
-                                    etsitäänid++;
-                                }
-
-                                Toast.makeText(getBaseContext(), "You have selected " + imageArray[etsitäänid].getTag() + System.lineSeparator() +
-                                        "Description: " + descriptionArray[etsitäänid] + System.lineSeparator() +
-                                        "ID: " + idArray[etsitäänid], Toast.LENGTH_SHORT).show();
-                            }
-                        });
-
-                        textArray[i].setOnClickListener(new View.OnClickListener() {
-                            public void onClick(View v) {
-
-                                String tagi = v.getTag().toString();
-                                int etsitäänid = 0;
-
-                                while(tagi != textArray[etsitäänid].getTag())
-                                {
-                                    etsitäänid++;
-                                }
-
-                                Toast.makeText(getBaseContext(), "You have selected " + imageArray[etsitäänid].getTag() + System.lineSeparator() +
-                                        "Description: " + descriptionArray[etsitäänid] + System.lineSeparator() +
-                                        "ID: " + idArray[etsitäänid], Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).execute("http://ec2-35-167-155-40.us-west-2.compute.amazonaws.com/foods");
+        getBoth(buttonfood);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -182,6 +88,236 @@ public class ScrollingActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public void addDrinksToFoods() {
+        JsonTask asyncTask = (JsonTask) new JsonTask(new JsonTask.AsyncResponse() {
+            @Override
+            public void processFinish(String output) {
+
+                try {
+                    JSONArray json = new JSONArray(output);
+                    layoutParamsImage.topMargin = 100;
+                    drinkamount = json.length();
+
+                    for(int i = 0; i < drinkamount; i++) {
+                        JSONObject jsonobj = json.getJSONObject(i);
+
+                        imageurl = jsonobj.getString("IMG_URL");
+                        description = jsonobj.getString("description");
+                        name = jsonobj.getString("drink_name");
+                        id = jsonobj.getInt("id");
+
+                        indeksi = i+foodamount;
+                        LinearLayout mainLayout = (LinearLayout) findViewById(R.id.scrollingLayout);
+
+                        ImageView foodimage = new ImageView(ScrollingActivity.this);
+                        imageArray[indeksi] = foodimage;
+
+                        TextView foodtext = new TextView(ScrollingActivity.this);
+                        foodtext.setText(name);
+                        textArray[indeksi] = foodtext;
+
+                        imageArray[indeksi].setLayoutParams(layoutParamsImage);
+                        mainLayout.addView(imageArray[indeksi]);
+
+                        textArray[indeksi].setLayoutParams(layoutParamsText);
+                        textArray[indeksi].setGravity(Gravity.CENTER_HORIZONTAL);
+                        mainLayout.addView(textArray[indeksi]);
+
+                        textArray[indeksi].setText(name);
+                        textArray[indeksi].setTag(name);
+                        imageArray[indeksi].setTag(name);
+                        textArray[indeksi].setId(id);
+
+                        idArray[indeksi] = id;
+                        descriptionArray[indeksi] = description;
+                        imgurlArray[indeksi] = (imageurl);
+
+                        Picasso.with(ScrollingActivity.this).load(imgurlArray[indeksi]).into(imageArray[indeksi]);
+                        setOnClickListeners();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).execute("https://shrouded-oasis-85914.herokuapp.com/drinks");
+    }
+
+
+    public void getBoth(View v) {
+        getFoods(buttonfood);
+        addDrinksToFoods();
+    }
+
+
+
+
+
+
+    public void getDrinks(View v) {
+
+        emptyViews();
+
+        JsonTask asyncTask = (JsonTask) new JsonTask(new JsonTask.AsyncResponse() {
+            @Override
+            public void processFinish(String output) {
+
+                try {
+                    JSONArray json = new JSONArray(output);
+                    layoutParamsImage.topMargin = 100;
+                    drinkamount = json.length();
+
+                    for(int i = 0; i < foodamount; i++) {
+                        JSONObject jsonobj = json.getJSONObject(i);
+
+                        imageurl = jsonobj.getString("IMG_URL");
+                        description = jsonobj.getString("description");
+                        name = jsonobj.getString("drink_name");
+                        id = jsonobj.getInt("id");
+
+                        indeksi = i;
+                        setTextsAndImages();
+                        setOnClickListeners();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).execute("https://shrouded-oasis-85914.herokuapp.com/drinks");
+    }
+
+
+
+
+
+
+    public void getFoods(View v) {
+
+        emptyViews();
+
+        JsonTask asyncTask = (JsonTask) new JsonTask(new JsonTask.AsyncResponse() {
+            @Override
+            public void processFinish(String output) {
+
+                try {
+                    JSONArray json = new JSONArray(output);
+                    layoutParamsImage.topMargin = 100;
+                    foodamount = json.length();
+
+                    for(int i = 0; i < foodamount; i++) {
+                        JSONObject jsonobj = json.getJSONObject(i);
+
+                        imageurl = jsonobj.getString("IMG_URL");
+                        description = jsonobj.getString("description");
+                        name = jsonobj.getString("food_name");
+                        id = jsonobj.getInt("id");
+
+                        indeksi = i;
+                        setTextsAndImages();
+                        setOnClickListeners();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).execute("https://shrouded-oasis-85914.herokuapp.com/foods");
+    }
+
+
+
+
+
+
+    public void setTextsAndImages() {
+        LinearLayout mainLayout = (LinearLayout) findViewById(R.id.scrollingLayout);
+
+        ImageView foodimage = new ImageView(ScrollingActivity.this);
+        imageArray[indeksi] = foodimage;
+
+        TextView foodtext = new TextView(ScrollingActivity.this);
+        foodtext.setText(name);
+        textArray[indeksi] = foodtext;
+
+        imageArray[indeksi].setLayoutParams(layoutParamsImage);
+        mainLayout.addView(imageArray[indeksi]);
+
+        textArray[indeksi].setLayoutParams(layoutParamsText);
+        textArray[indeksi].setGravity(Gravity.CENTER_HORIZONTAL);
+        mainLayout.addView(textArray[indeksi]);
+
+        textArray[indeksi].setText(name);
+        textArray[indeksi].setTag(name);
+        imageArray[indeksi].setTag(name);
+        textArray[indeksi].setId(id);
+
+        idArray[indeksi] = id;
+        descriptionArray[indeksi] = description;
+        imgurlArray[indeksi] = (imageurl);
+
+        Picasso.with(ScrollingActivity.this).load(imgurlArray[indeksi]).into(imageArray[indeksi]);
+    }
+
+
+
+
+
+
+
+    public void setOnClickListeners() {
+
+        imageArray[indeksi].setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                tagi = v.getTag().toString();
+                clickityclick();
+            }
+        });
+
+        textArray[indeksi].setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                tagi = v.getTag().toString();
+                clickityclick();
+            }
+        });
+    }
+
+
+
+
+
+
+
+    public void clickityclick() {
+        int etsitäänid = 0;
+
+        while(tagi != textArray[etsitäänid].getTag())
+        {
+            etsitäänid++;
+        }
+
+        Toast.makeText(getBaseContext(), "You have selected " + imageArray[etsitäänid].getTag() + System.lineSeparator() +
+                "Description: " + descriptionArray[etsitäänid] + System.lineSeparator() +
+                "ID: " + idArray[etsitäänid], Toast.LENGTH_SHORT).show();
+    }
+
+
+
+
+    public void emptyViews() {
+        for(int i = 0; i < foodamount+drinkamount; i++) {
+            LinearLayout mainLayout = (LinearLayout) findViewById(R.id.scrollingLayout);
+            mainLayout.removeView(textArray[i]);
+            mainLayout.removeView(imageArray[i]);
+        }
+
+        imageArray = new ImageView[100];
+        textArray = new TextView[100];
+        descriptionArray = new String[100];
+        imgurlArray = new String[100];
+        idArray = new int[100];
     }
 
 

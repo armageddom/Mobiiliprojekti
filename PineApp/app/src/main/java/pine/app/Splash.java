5cb5f0035c6c30194214d943561714c19d2d121b
tstenbacka <tomi.stenbacka@gmail.com>
2017-04-11 11:45:09 +0300

    moar UI

--- PineApp/app/src/main/java/pine/app/Splash.java
+++ PineApp/app/src/main/java/pine/app/Splash.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Splash extends AppCompatActivity {
     /** Duration of wait **/
@@ -12,7 +14,10 @@ public class Splash extends AppCompatActivity {
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
 
         /* New Handler to start the Menu-Activity
          * and close this Splash-Screen after some seconds.*/
